7888c759e58bf96f3fb70dc0fb93abc7036a9a6b
elgeiger2 <elgeiger2@wisc.edu>
2024-04-29 20:40:20 -0500

    Added D3  Library
    added d3 lib and created map element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 //insert code here!
+window.onload = setMap();
+
+//Example 1.3 line 4...set up choropleth map
+function setMap() {
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3
+        .select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+        
+    }
